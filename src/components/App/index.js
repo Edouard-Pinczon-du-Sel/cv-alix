74bd17746d471af57765dff1bf25b4d358d04734
Edouard-Pinczon-du-Sel <edouardpinczon@gmai.com>
2022-08-11 16:28:07 +0200

    test

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+// == Import
+import AppHeader from '../AppHeader';
+import './styles.scss';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      <AppHeader />
+    </div>
+  );
+}
+
+// == Export
+export default App;
---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,20 @@
+// == Import
+import './styles.scss';
+
+// == Composant
+function AppHeader() {
+  return (
+    <div className="header">
+      <nav>
+        <a href="#">Presensation</a>
+        <a href="#">Expérience</a>
+        <a href="#">Compétences</a>
+        <a href="#">Contact</a>
+      </nav>
+      <img srx="#" />
+    </div>
+  );
+}
+
+// == Export
+export default AppHeader;
